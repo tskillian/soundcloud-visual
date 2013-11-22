7b4956128cfb285e25aa9988785f0c0176712394
Tim Killian <tskillian@gmail.com>
2013-11-21 19:09:36 -0800

    cleaning up router

--- js/routers/router.js
+++ js/routers/router.js
@@ -1,49 +1,16 @@
 APP.Router = Backbone.Router.extend({
 	routes: {
-		"first": "firstRoute",
-		"second": "secondRoute",
-		"play": "play",
-		"": "songs"
+		"": "songs",
 	},
 
-	firstRoute: function() {
-		console.log("firstRoute() was hit.");
-		APP.usersCollection = new APP.Users();
-		APP.usersCollection.create({
-			name: "colin",
-			phone: "234-234-2342"
-		});
-		APP.usersCollection.create({
-			name: "dan",
-			address: "Seattle"
-		});
-	},
-
-	secondRoute: function() {
-
-		APP.usersCollection = new APP.Users();
-		APP.usersCollection.fetch();
-		console.dir(APP.usersCollection);
-		console.log("secondRoute() was hit");
-	},
 
 	songs: function() {
 		APP.songsCollection = new APP.Songs();
 		var newView = new APP.SearchResultsView({
-			collection: APP.songsCollection
+			
 		});
 		newView.render();
 		$(document.body).append(newView.$el);
-	},
-
-	play: function() {
-		console.log('play route hit');
-		APP.song = new APP.Song();
-		var playView = new APP.PlayView({
-			model: APP.song
-		});
-		playView.render();
-		$(document.body).append(playView.$el);
 	}
 });
 
